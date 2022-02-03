b0c9a38c1e559df6327fe2f5b6d69eb4fdd7bd96
Emilie Martin <emilie.martindonati@gmail.com>
2022-02-03 17:13:16 +0100

    client sign journey

--- src/Pages/AuthSignup.jsx
+++ src/Pages/AuthSignup.jsx
@@ -1,7 +1,7 @@
 // import FormInput from "../Components/FormInput";
 // import useForm from "../Hooks/useForm";
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import axios from "axios";
 
 const AuthSignup = ({ role }) => {
@@ -22,7 +22,7 @@ const AuthSignup = ({ role }) => {
             .post("http://localhost:4000/signup", values)
             .then((infos) => {
                 console.log("This is coming back from the server !", infos.data);
-                navigate("/login");
+                // navigate("/login", <AuthSignin role={role} />);
             })
             .catch((error) => {
                 console.log(error);
@@ -30,7 +30,18 @@ const AuthSignup = ({ role }) => {
     };
 
     return (
-        <h1>FORM</h1>
+        <>
+        <form onSubmit={handleSubmit} onChange={handleChange}>
+            <label htmlFor='name'>NAME</label>
+            <input type="text" id="name" required/>
+            <label htmlFor='email'>EMAIL</label>
+            <input type="text" id="email" required/>
+            <label htmlFor='password'>PASSWORD</label>
+            <input type="password" id="password" required/>
+           <button><h5>SIGN UP</h5></button>
+        </form>
+        <Navlink to="/login" role={role}><h3>Already registered ? Log in</h3></Navlink>
+        </>
     );
 };
 
