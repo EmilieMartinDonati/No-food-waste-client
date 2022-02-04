f40ab088bb696ed9e1f5390fd00462aaa5ad43e2
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-02-04 11:01:51 +0100

    Modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import useAuth from "./../context/UseAuth";
 
 const Navbar = () => {
+  const { loggedIn, currentUser } = useAuth;
+
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/listings">Search</NavLink>
-      <NavLink to="/favorites">Favorites</NavLink>
-      <NavLink to="/dashboard">Dashboard</NavLink>
-      <NavLink to="/account">My account</NavLink>
-      <NavLink to="/login">Log in</NavLink>
-      <NavLink to="/signup">Sign up</NavLink>
-      <NavLink to="/logout">Log out</NavLink>
+      {!loggedIn && (
+        <>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/login">Log in</NavLink>
+          <NavLink to="/signup">Sign up</NavLink>
+        </>
+      )}
+
+      {loggedIn && currentUser.role === "user" && (
+        <>
+          <NavLink to="/listings">Search</NavLink>
+          <NavLink to="/favorites">Favorites</NavLink>
+          <NavLink to="/account">My account</NavLink>
+          <NavLink to="/logout">Log out</NavLink>
+        </>
+      )}
+
+      {loggedIn && currentUser.role === "business" && (
+        <NavLink to="/dashboard">Dashboard</NavLink>
+      )}
     </nav>
   );
 };
