d347f2f2b70745f7e68376427c5c66342fc6e5aa
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-02-08 19:43:04 +0100

    Changes to discover and listingcard components

--- src/Pages/Discover/Browse.jsx
+++ src/Pages/Discover/Browse.jsx
@@ -3,13 +3,12 @@ import apiHandler from "../../api/apiHandler";
 import ListingCard from "../../components/ListingCard";
 import { NavLink } from "react-router-dom";
 import Geocode from "react-geocode";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 Geocode.setApiKey("AIzaSyAWNUhMz1o6js88esl8_xmRkQgFOZr38nk");
 Geocode.setLanguage("fr");
 
 const Browse = () => {
-
   const [latitudeUser, setLatitudeUser] = useState(0);
   const [longitudeUser, setLongitudeUser] = useState(0);
   const [listings, setListings] = useState([]);
@@ -20,24 +19,24 @@ const Browse = () => {
 
   const [search, setSearch] = useState("");
 
-
   const containerStyle = {
-    width: '300px',
-    height: '300px',
+    width: "300px",
+    height: "300px",
     marginLeft: "3%",
   };
 
-
-
-
-  // The code below is a parser that should work for adresses for most countries. 
+  // The code below is a parser that should work for adresses for most countries.
   // We can find where the current user is this wayyy !
   Geocode.fromLatLng(latitudeUser, longitudeUser).then(
     (response) => {
       const address = response.results[0].formatted_address;
       let city, state, country;
       for (let i = 0; i < response.results[0].address_components.length; i++) {
-        for (let j = 0; j < response.results[0].address_components[i].types.length; j++) {
+        for (
+          let j = 0;
+          j < response.results[0].address_components[i].types.length;
+          j++
+        ) {
           switch (response.results[0].address_components[i].types[j]) {
             case "locality":
               city = response.results[0].address_components[i].long_name;
@@ -59,9 +58,7 @@ const Browse = () => {
     }
   );
 
-
   useEffect(() => {
-
     if ("geolocation" in navigator) {
       console.log("Available");
     } else {
@@ -73,19 +70,19 @@ const Browse = () => {
       console.log("Longitude is :", position.coords.longitude);
       setLongitudeUser(position.coords.longitude);
     });
-    apiHandler.get("/discover").then((res) => {
-      console.log(res.data);
-      setListings(res.data);
-      console.log(listings);
-    }
-    )
+    apiHandler
+      .get("/listings")
+      .then((res) => {
+        console.log(res.data);
+        setListings(res.data);
+        console.log(listings);
+      })
       .catch((e) => console.log(e));
-  }, [])
-
+  }, []);
 
   useEffect(() => {
-    initLocalisations()
-  }, [listings])
+    initLocalisations();
+  }, [listings]);
 
   const fetchGeocode = (listing) => {
     return new Promise(async (resolve, reject) => {
@@ -95,13 +92,14 @@ const Browse = () => {
         const { lat, lng } = APIres.results[0].geometry.location;
         console.log("lat long", lat, lng);
         resolve({ lat, lng });
+      } catch (e) {
+        reject(e);
       }
-      catch (e) { reject(e) }
-    })
-  }
+    });
+  };
 
   const initLocalisations = async () => {
-    console.log("waht is in listings ?", listings)
+    console.log("waht is in listings ?", listings);
     try {
       for (let listing of listings) {
         //console.log(!!listing, listing)
@@ -111,45 +109,46 @@ const Browse = () => {
         console.log("this is the listing with the coordinates", listing);
       }
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   console.log("listings, I hope they have the coordinates", listings);
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   const handleSubmit2 = (e) => {
     e.preventDefault();
-  }
+  };
 
   const handleSearch = (e) => {
     e.preventDefault();
     const data = {
-      search: search
-    }
+      search: search,
+    };
     // apiHandler.post("/categories", data)
     // .then((dbRes) => console.log(dbRes))
     // .catch((e) => console.log(e))
-  }
+  };
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-12">
           <h1 className="text-center text-danger">BROWSE</h1>
-          <p>Offers near <span style={{ color: "slategrey" }}>{userAddress}</span></p>
+          <p>
+            Offers near{" "}
+            <span style={{ color: "slategrey" }}>{userAddress}</span>
+          </p>
         </div>
       </div>
 
       <div className="row">
         <div className="col-12">
           <form onSubmit={handleSearch}>
-            <label htmlFor="search">
-            </label>
+            <label htmlFor="search"></label>
             <input
               type="text"
               id="search"
@@ -164,72 +163,81 @@ const Browse = () => {
       <div className="row">
         <div className="col-6">
           <form onSubmit={handleSubmit}>
-            <input type="text" name="list" value="list" style={{
-              visibility: "hidden"
-            }} />
-            <button className="btn-info" onClick={(e) => setMapOrList(e.target.value)}>LIST</button>
+            <input
+              type="text"
+              name="list"
+              value="list"
+              style={{
+                visibility: "hidden",
+              }}
+            />
+            <button
+              className="btn-info"
+              onClick={(e) => setMapOrList(e.target.value)}
+            >
+              LIST
+            </button>
           </form>
         </div>
         <div className="col-6">
           <form onSubmit={handleSubmit2}>
-            <input type="text" name="map" value="map" style={{
-              visibility: "hidden"
-            }} />
-            <button className="btn-info" onClick={(e) => setMapOrList(e.target.value)}>MAP</button>
+            <input
+              type="text"
+              name="map"
+              value="map"
+              style={{
+                visibility: "hidden",
+              }}
+            />
+            <button
+              className="btn-info"
+              onClick={(e) => setMapOrList(e.target.value)}
+            >
+              MAP
+            </button>
           </form>
         </div>
       </div>
 
       <div className="row">
-
         <div className="col-6 d-flex-column justify-content-center">
-
           {listings.map((listing) => {
-            {/* console.log("this is the log of the listing", listing._id); */ }
+            {
+              /* console.log("this is the log of the listing", listing._id); */
+            }
             return (
               <div key={listing._id}>
                 <ListingCard listing={listing} />
                 {/* <NavLink to={`/listing/${listing._id}`}><h4>Interested by this offer ? Click to learn more and conclude your reservation</h4></NavLink> */}
               </div>
-            )
-          })
-          }
-
+            );
+          })}
         </div>
-
 
         <div className="col-6 d-flex-column justify-content-center">
           <p className="text-uppercase">See on card</p>
           {listings.map((listing) => {
-            console.log("gimme the damn coord :'(", listing)
+            console.log("gimme the damn coord :'(", listing);
             const center = {
               lat: listing.coord?.lat,
               lng: listing.coord?.lng,
-            }
+            };
             return (
               <>
                 <h4>{listing.name}</h4>
                 <p>{listing.owner.address}</p>
                 <p>{listing.coord?.lat}</p>
                 <p>{listing.coord?.lng}</p>
-                <LoadScript
-                  googleMapsApiKey="AIzaSyAWNUhMz1o6js88esl8_xmRkQgFOZr38nk"
-                >
+                <LoadScript googleMapsApiKey="AIzaSyAWNUhMz1o6js88esl8_xmRkQgFOZr38nk">
                   <GoogleMap
                     mapContainerStyle={containerStyle}
                     center={center}
                     zoom={20}
-                  >
-                  </GoogleMap>
+                  ></GoogleMap>
                 </LoadScript>
               </>
-            )
-          })
-          }
-
-
-
-
+            );
+          })}
         </div>
       </div>
     </div>
--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,49 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import apiHandler from "../API/APIHandler";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as faHeartFull } from "@fortawesome/free-solid-svg-icons";
 
 const ListingCard = ({ listing }) => {
-  // !!!!
-  //! Note for ourselves: to use listing.owner.picture on lines 10, 12 and 14, it means that the listing object sent through prop must have had been sent with populate on owner
-  // !!!!
+  const [favIcon, setFavIcon] = useState(faHeart);
+
+  const toggleFavorite = () => {
+    // Send a patch route for the user object
+    apiHandler
+      .patch(`/account/favorites/${listing.owner._id}`)
+      .then((dbRes) => {
+        console.log("This is dbRes from line 32 of ListingCard", dbRes);
+        // Change the favorite setting visually depending on dbRes
+        dbRes.data.favorites.includes(listing._id)
+          ? setFavIcon(faHeartFull)
+          : setFavIcon(faHeart);
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
-
     <div className="card" style={{ width: "30rem" }}>
-      <img className="card-img-top" src={listing.owner.picture} alt={listing.name} />
+      <img
+        className="card-img-top"
+        src={listing.owner.picture}
+        alt={listing.name}
+      />
       <div className="card-body">
         <h5 className="card-title">{listing.name}</h5>
         <h5 className="card-text">Price: {listing.price}€</h5>
         <p className="card-text">{listing.owner.name}</p>
-        <FontAwesomeIcon
-          icon={faHeartFull}
-          size="xs"
-        />
+        <FontAwesomeIcon onClick={toggleFavorite} icon={favIcon} size="xs" />
         <p className="card-text">Pick up by {listing.owner.pickupTimeSlots}</p>
         <p>{listing.availableQuantity}</p>
         <Link to={`/listing/${listing._id}`}>See this offer</Link>
-
       </div>
     </div>
-
-    /* <div>
-    <Link to={`/listing/${listing._id}`}>See this offer</Link>
-      <img src={listing.owner.picture} alt={listing.name} />
-      <div>
-        <div>
-          <h2>{listing.owner.name}</h2>
-          <h3>{listing.name}</h3>
-          <p>Pick up by {listing.owner.pickupTimeSlots}</p>
-        </div>
-        <div>
-          <p>Price: {listing.price}€</p>
-        </div>
-      </div>
-    </div> */
-
-
   );
 };
 
